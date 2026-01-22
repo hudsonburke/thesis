a582837f4b56632a9bb5b29828d4ad71a2c5be0c
Hudson Burke <hpb7kr@virginia.edu>
2026-01-22 14:22:34 -0500

    Restructure

---
+++ _extensions/mcanouil/external/external.lua
@@ -0,0 +1,139 @@
+--[[
+# MIT License
+#
+# Copyright (c) 2026 Mickaël Canouil
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+]]
+
+--- Extension name constant
+local EXTENSION_NAME = 'external'
+
+--- Load utils, validation and content-extraction modules
+local utils = require(quarto.utils.resolve_path('_modules/utils.lua'):gsub('%.lua$', ''))
+local validation = require(quarto.utils.resolve_path('_modules/validation.lua'):gsub('%.lua$', ''))
+local content = require(quarto.utils.resolve_path('_modules/content-extraction.lua'):gsub('%.lua$', ''))
+
+--- Includes external content or a section/div from a file into a Pandoc document.
+--- Supports including entire markdown files, specific sections identified by header IDs,
+--- or divs identified by their IDs.
+--- The URI can contain a hash fragment (#id) to include only that section or div.
+--- For .qmd files, uses Quarto's string_to_blocks parser.
+--- For other markdown files, uses Pandoc's reader with shortcode escaping.
+---
+--- @param args table Arguments array where first element is the file URI (with optional #id)
+--- @param _kwargs table Named keyword arguments (unused)
+--- @param _meta table Document metadata (unused)
+--- @param _raw_args table Raw arguments (unused)
+--- @param _context table Context information (unused)
+--- @return table Included content blocks or pandoc.Null() on error
+--- @usage {{< external path/to/file.md#section-id >}}
+--- @usage {{< external path/to/file.md#div-id >}}
+local function include_external(args, _kwargs, _meta, _raw_args, _context)
+  --- @type string File URI to include
+  local uri = pandoc.utils.stringify(args[1])
+  --- @type string|nil Optional section or div identifier from hash fragment
+  local element_id = nil
+  --- @type integer|nil Position of hash character in URI
+  local hash_index = uri:find('#')
+  if hash_index then
+    element_id = uri:sub(hash_index + 1)
+    uri = uri:sub(1, hash_index - 1)
+  end
+
+  -- Use validation module to check markdown extension
+  if not validation.is_markdown(uri) then
+    utils.log_warning(
+      EXTENSION_NAME,
+      'Only markdown files (.md, .markdown, .qmd) are supported. ' ..
+      'The file \'' .. uri .. '\' will not be included.'
+    )
+    return pandoc.Null()
+  end
+
+  --- @type string|nil MIME type of the fetched file (unused but returned by fetch)
+  --- @type string|nil File contents as string
+  local _mt, contents = pandoc.mediabag.fetch(uri)
+  if not contents then
+    utils.log_error(
+      EXTENSION_NAME,
+      'Could not open file \'' .. uri .. '\'. ' ..
+      'Please check that the file path is correct and the file is accessible.'
+    )
+    return pandoc.Null()
+  end
+
+  --- @type table Pandoc blocks parsed from file contents
+  local contents_blocks
+
+  -- If element_id is provided, strip YAML frontmatter before parsing
+  if element_id then
+    --- @type string Content without YAML frontmatter
+    local content_without_yaml = contents
+    local yaml_start = content_without_yaml:match('^%s*%-%-%-')
+    if yaml_start then
+      local _, yaml_end = content_without_yaml:find('\n%-%-%-%s*\n', 1, false)
+      if yaml_end then
+        content_without_yaml = content_without_yaml:sub(yaml_end + 1)
+      end
+    end
+
+    if uri:lower():match('%.qmd$') then
+      contents_blocks = quarto.utils.string_to_blocks(content_without_yaml)
+    else
+      content_without_yaml = content_without_yaml:gsub('({{<.-[ \t]>}})', '{%1}')
+      contents_blocks = pandoc.read(content_without_yaml).blocks
+    end
+  else
+    if uri:lower():match('%.qmd$') then
+      contents_blocks = quarto.utils.string_to_blocks(contents)
+    else
+      contents = contents:gsub('({{<.-[ \t]>}})', '{%1}')
+      contents_blocks = pandoc.read(contents).blocks
+    end
+  end
+
+  if element_id then
+    local section_blocks = content.extract_section(contents_blocks, element_id, true)
+    if section_blocks then
+      return pandoc.Blocks(section_blocks)
+    end
+
+    local div_blocks = content.extract_div(contents_blocks, element_id, false)
+    if div_blocks then
+      return pandoc.Blocks(div_blocks)
+    end
+
+    utils.log_error(
+      EXTENSION_NAME,
+      'Section or div \'#' .. element_id .. '\' not found in \'' .. uri .. '\'. ' ..
+      'Please check that the identifier matches a header or div in the file.'
+    )
+    return pandoc.Null()
+  end
+
+  return contents_blocks
+end
+
+--- Module export table.
+--- Defines the shortcode available to Quarto for including external content.
+--- @type table<string, function>
+return {
+  ['external'] = include_external
+}
